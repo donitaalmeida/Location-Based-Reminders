2875b0dc0cb5eb1616f7b0effbcfcf3b1446ca0b
donitaalmeida <donita.almeida@sjsu.edu>
2016-07-30 22:20:10 -0700

    Network Connection Detector

---
+++ app/src/main/java/com/bignerdranch/android/locationbasedreminders/NetworkConnectionDetector.java
@@ -0,0 +1,33 @@
+package com.bignerdranch.android.locationbasedreminders;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by donita on 25-07-2016.
+ * check if internet is available
+ */
+public class NetworkConnectionDetector {
+
+    private Context _context;
+
+    public NetworkConnectionDetector(Context context){
+        this._context = context;
+    }
+
+    public boolean isConnectingToInternet(){
+        ConnectivityManager connectivity = (ConnectivityManager) _context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivity != null) {
+            NetworkInfo[] info = connectivity.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED)
+                    {
+                        return true;
+                    }
+
+        }
+        return false;
+    }
+}
