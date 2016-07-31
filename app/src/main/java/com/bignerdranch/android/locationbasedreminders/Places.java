b5ca39bebddd867415043e49e6a92a720b93cd7b
donitaalmeida <donita.almeida@sjsu.edu>
2016-07-30 22:20:31 -0700

    Places added

---
+++ app/src/main/java/com/bignerdranch/android/locationbasedreminders/Places.java
@@ -0,0 +1,87 @@
+package com.bignerdranch.android.locationbasedreminders;
+
+/**
+ * Created by donita on 11-07-2016.
+ * object to hold list of properties for places
+ */
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Places {
+
+    public List<HashMap<String, String>> parse(JSONObject jsonObject) {
+        JSONArray jsonArray = null;
+        try {
+            jsonArray = jsonObject.getJSONArray("results");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return getPlaces(jsonArray);
+    }
+
+    private List<HashMap<String, String>> getPlaces(JSONArray jsonArray) {
+        int placesCount = jsonArray.length();
+        List<HashMap<String, String>> placesList = new ArrayList<HashMap<String, String>>();
+        HashMap<String, String> placeMap = null;
+        for (int i = 0; i < placesCount; i++) {
+            try {
+                placeMap = getPlace((JSONObject) jsonArray.get(i));
+                placesList.add(placeMap);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return placesList;
+    }
+
+    private HashMap<String, String> getPlace(JSONObject googlePlaceJson) {
+        HashMap<String, String> googlePlaceMap = new HashMap<String, String>();
+        String placeName = "-NA-";
+        String vicinity = "-NA-";
+        String latitude = "";
+        String longitude = "";
+        String reference = "";
+        String ratings="0";
+        String price_level="0";
+        String open_now="false";
+
+        try {
+            if (!googlePlaceJson.isNull("name")) {
+                placeName = googlePlaceJson.getString("name");
+            }
+            if (!googlePlaceJson.isNull("vicinity")) {
+                vicinity = googlePlaceJson.getString("vicinity");
+            }
+            if(!googlePlaceJson.isNull("rating")){
+                ratings=googlePlaceJson.getString("rating");
+            }
+            if(!googlePlaceJson.isNull("price_level")){
+                price_level=googlePlaceJson.getString("price_level");
+            }
+            if(!googlePlaceJson.isNull("opening_hours")){
+                open_now=googlePlaceJson.getJSONObject("opening_hours").getString("open_now");
+            }
+            latitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lat");
+            longitude = googlePlaceJson.getJSONObject("geometry").getJSONObject("location").getString("lng");
+            reference = googlePlaceJson.getString("reference");
+            googlePlaceMap.put("place_name", placeName);
+            googlePlaceMap.put("vicinity", vicinity);
+            googlePlaceMap.put("lat", latitude);
+            googlePlaceMap.put("lng", longitude);
+            googlePlaceMap.put("reference", reference);
+            googlePlaceMap.put("ratings",  ratings);
+            googlePlaceMap.put("price_level",price_level);
+            googlePlaceMap.put("open_now",open_now);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return googlePlaceMap;
+    }
+}
